76a7fb8da78c53e16d6aabfa0d93da2d0e57a536
SeddikEsprit <seddik.bouzayani@esprit.tn>
2022-02-08 11:19:50 +0100

    workshop_07/02/2022

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Student;
+use App\Form\StudentType;
+use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -44,5 +47,38 @@ class StudentController extends AbstractController
         $em->remove($student);
         $em->flush();
         return $this->redirectToRoute("students");
+    }
+    /**
+     * @Route("/addStudent/", name="addStudent")
+     */
+    public function addStudent(Request $request){
+        $student=new Student();
+        $form=$this->createForm(StudentType::class,$student);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($student);
+            $em->flush();
+            return $this->redirectToRoute("students");
+
+        }
+
+        return $this->render("Student/addStudent.html.twig",array('formulaireStudent'=>$form->createView()));
+    }
+    /**
+     * @Route("/updateStudent/{id}", name="updateSudent")
+     */
+    public function updateStudent(Request $request,StudentRepository $repository,$id){
+        $student=$repository->find($id);
+        $form=$this->createForm(StudentType::class,$student);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em=$this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute("students");
+
+        }
+
+        return $this->render("Student/updateStudent.html.twig",array('formulaireUpdateStudent'=>$form->createView()));
     }
 }
